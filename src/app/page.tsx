1845d015173cce746f173049ba610ae67ddf93fd
hbashar434 <h.bashar434@gmail.com>
2024-10-08 17:19:07 +0600

    delete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import FileUpload from "@/components/FileUpload";
 import Navbar from "@/components/Navbar";
 
 const HomePage = () => {
+  console.log(process.env.MONGODB_URI);
+
   return (
     <section>
       <Navbar />
